d516531428ab2c60a53f09667bad636fe73a40e2
SynergyShadow <36507966+ShadowTigerCool@users.noreply.github.com>
2026-04-09 10:13:24 -0400

    Update Helper.java

--- spigot/src/main/java/org/dynmap/bukkit/Helper.java
+++ spigot/src/main/java/org/dynmap/bukkit/Helper.java
@@ -61,6 +61,9 @@ public class Helper {
             else if (v.contains("(MC: 1.21.")) {	// Set up in case 1.21.12 works 'as is'
 	            BukkitVersionHelper.helper = loadVersionHelper("org.dynmap.bukkit.helper.v121_11.BukkitVersionHelperSpigot121_11");
             }
+			else if (v.contains("(MC: 26.")) {	// Set up in case 1.21.12 works 'as is'
+	            BukkitVersionHelper.helper = loadVersionHelper("org.dynmap.bukkit.helper.v26_1.BukkitVersionHelperSpigot26_1");
+			}
             else if (v.contains("(MC: 1.20)") || v.contains("(MC: 1.20.1)")) {
             	BukkitVersionHelper.helper = loadVersionHelper("org.dynmap.bukkit.helper.v120.BukkitVersionHelperSpigot120");
             }
